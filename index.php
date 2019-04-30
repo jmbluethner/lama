b87add68914c26d38f9a25563719f4fecfe7ea93
nighttimedev <39534549+nighttimedev@users.noreply.github.com>
2019-04-30 14:45:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 // Check PHP Session Login-State
 // if loggedin == 1 => interface.php else => login.php
-
-<?php
-
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include "./web-elements/php/head.php" ?>
+    <?php
+      if($_SESSION['loginstate']=='user' || $_SESSION['loginstate']=='admin') {
+        header("Location: interface.php");
+        die();
+      } else {
+        header("Location: login.php");
+      }
+    ?>
+  </head>
+  <body>
+    Checking Login-State ...
+  </body>
+</html>
