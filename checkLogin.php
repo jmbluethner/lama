88cfaf1f46116ecd34268fb2741834c53d42c3f3
jamo <mail@jan-morris.de>
2019-04-30 22:57:38 +0200

    Update Login-System

--- checkLogin.php
+++ checkLogin.php
@@ -1,3 +1,13 @@
-// This file checks if the login is valid, and rather the user is a 'user' or 'moderator'.
-// We use a SQL database to check the credentials.
-// Commit via Atom (Test)
+<?php
+
+  // This file checks if the login is valid, and rather the user is a 'user' or 'moderator'.
+  // We use a SQL database to check the credentials.
+
+  $globalSettings = inlude('rushb.conf');
+
+  $mail = $_GET['loginMail'];
+  $password = $_GET['loginPassword'];
+
+  $sql = "SELECT mail FROM users WHERE mail='".$loginMail."'";
+
+?>
--- login.php
+++ login.php
@@ -1,1 +1,11 @@
+<form method="GET" action="checkLogin.php">
+  <input type="text" placeholder="mail"/>
+  <input type="password" placeholder="password"/>
+  <button type="submit">Sign Up</button>
+</form>
 
+<?php
+
+  // This page is where you can Enter your Login-Information, if any of the pages redirected to it
+
+?>
