6e5812a39822f375bb10c8972c4749225b2d4587
jamo <mail@jan-morris.de>
2019-05-03 13:09:59 +0200

    First Work on dashboard

--- dashboard.php
+++ dashboard.php
@@ -2,3 +2,16 @@
         This file is the First Page a logged-in user will see.
         Here you can see all Servers which where transmitted to the database.
  -->
+
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <?php include "./web-elements/php/header.php" ?>
+        </head>
+        <body>
+            <section class="mainframe">
+
+            </section>
+            <script src=""></script>
+        <body>
+    </html>
