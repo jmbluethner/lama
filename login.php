011ba3db76a535548d5a243cf8cf4b4846b8a504
root <mail@jan-morris.de>
2019-05-10 23:27:16 +0200

    Layout & Design Update on Login Page

--- login.php
+++ login.php
@@ -1,8 +1,18 @@
-<form method="POST" action="checkLogin.php">
-  <input name="loginMail" type="text" placeholder="mail"/>
-  <input name="loginPass" type="password" placeholder="password"/>
-  <button type="submit">Sign Up</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>RushB - PHP Server Interface</title>
+    <link rel="stylesheet" href="./assets/css/login.css"/>
+  </head>
+  <body>
+    <form method="POST" action="checkLogin.php">
+      <input name="loginMail" type="text" placeholder="mail"/>
+      <input name="loginPass" type="password" placeholder="password"/>
+      <button type="submit">Sign Up</button>
+    </form>
+  </body>
+</html>
 
 <?php
 
