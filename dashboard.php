e725e185337c07894590fa4a39ba8cca00043c81
root <mail@jan-morris.de>
2019-05-13 22:22:56 +0200

    dashboard style and html markup update

--- dashboard.php
+++ dashboard.php
@@ -4,18 +4,45 @@
  -->
 
 <!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <?php include "./web-elements/php/head.php" ?>
-        </head>
-        <body>
-            <section class="mainframe">
-                <div class="serverindex">
-                    <div class="servertile">
+<html lang="en">
 
-                    </div>
-                </div>
-            </section>
-            <script src=""></script>
-        </body>
-    </html>
+<head>
+  <?php include "./web-elements/php/head.php" ?>
+  <link rel="stylesheet" href="./web-elements/css/main.css" />
+</head>
+
+<body>
+  <nav>
+    <h1>CSGO Manager</h1>
+  </nav>
+  <section class="mainframe">
+    <div class="serverindex">
+      <div class="servertile">
+        <div class="content_upper">
+          <h2>Servername</h2>
+          <h6>[ID: 0001]</h6>
+          <table class="servertable">
+            <tr>
+              <th>Vac</th>
+              <th>Value</th>
+            </tr>
+            <tr>
+              <th>Mode</th>
+              <th>Value</th>
+            </tr>
+            <tr>
+              <th>Slots</th>
+              <th>Value</th>
+            </tr>
+          </table>
+        </div>
+        <div class="content_lower">
+          <button class="manage_server">Manage</button>
+        </div>
+      </div>
+    </div>
+  </section>
+  <script src=""></script>
+</body>
+
+</html>
