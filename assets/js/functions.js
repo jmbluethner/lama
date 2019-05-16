ea66d4bfaaf7e3bde528440c7372b4219557ccd9
Jamo <develop@nighttimedev.com>
2019-05-16 07:33:14 +0200

    further work on Dashboard

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,5 @@
+var paneheight;
+
 function goBack() {
   window.history.go(-1);
 }
@@ -5,14 +7,14 @@ function switchView(source) {
   document.getElementById('contentframe').src = "./plugins/"+source.id+".php";
 }
 function collapsePane(pane) {
-  if(document.getElementById(pane).style.height == "85px") {
+  if(document.getElementById(pane).style.height == "89px") {
     document.getElementById(pane).style.height = "auto";
     document.getElementById(pane+"_chevron").style.transform = "rotate(-180deg)";
   } else {
-    document.getElementById(pane).style.height = "85px";
+    document.getElementById(pane).style.height = "89px";
     document.getElementById(pane+"_chevron").style.transform = "rotate(0deg)";
   }
 }
 function createAlert(type,heading,content) {
-  
+
 }
--- plugins/home.php
+++ plugins/home.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="../assets/css/dashboard.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.1/css/all.css" type="text/css">
   </head>
-  <body>
+  <body class="framebody">
     <h1>Home</h1>
     <div class="pane">
       <h2>Welcome Username!</h2>
@@ -25,8 +25,55 @@
           <th>Maps</th>
           <th>Status</th>
         </tr>
+        <tr>
+          <td>test</td>
+          <td>test</td>
+          <td>test</td>
+          <td>test</td>
+          <td>test</td>
+        </tr>
+        <tr>
+          <td>test</td>
+          <td>test</td>
+          <td>test</td>
+          <td>test</td>
+          <td>test</td>
+        </tr>
+      </table>
+    </div>
+    <div class="pane" id="pane_recentActions">
+      <h3>Recent Actions</h3>
+      <button class="pane_collapse" onclick="collapsePane('pane_recentActions')">
+        <i class="fas fa-chevron-up" id="pane_recentActions_chevron"></i>
+      </button>
+      <table class="twocol_table">
+        <tr>
+          <td>Username banned 2nd Username because of bug abuse<p class="timeChanged">10 Minutes ago</p></td>
+          <td><button class="button_simple">revert</button></td>
+        </tr>
+        <tr>
+          <td>Username banned 2nd Username because of bug abuse<p class="timeChanged">10 Minutes ago</p></td>
+          <td><button class="button_simple">revert</button></td>
+        </tr>
+        <tr>
+          <td>Username banned 2nd Username because of bug abuse<p class="timeChanged">10 Minutes ago</p></td>
+          <td><button class="button_simple">revert</button></td>
+        </tr>
+        <tr>
+          <td>Username banned 2nd Username because of bug abuse<p class="timeChanged">10 Minutes ago</p></td>
+          <td><button class="button_simple">revert</button></td>
+        </tr>
+        <tr>
+          <td>Username banned 2nd Username because of bug abuse<p class="timeChanged">10 Minutes ago</p></td>
+          <td><button class="button_simple">revert</button></td>
+        </tr>
+        <tr>
+          <td>Username banned 2nd Username because of bug abuse<p class="timeChanged">10 Minutes ago</p></td>
+          <td><button class="button_simple">revert</button></td>
+        </tr>
       </table>
     </div>
     <script src="../assets/js/functions.js"></script>
+    <script src="https://cdn.nighttimedev.com/toolbox/js/generalFunctions.js"></script>
   </body>
 </html>
