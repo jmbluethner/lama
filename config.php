75aee0639b467d04e0b4999ea434ed932aa69bec
Jamo <develop@nighttimedev.com>
2019-05-21 17:05:23 +0200

    test config

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
   return [
-    'SQLhost' => 'mysql05.manitu.net',
+    'SQLhost' => 'mysql05.manitu.ne',
     'SQLdbname' => 'db31051',
     'SQLuser' => 'u31051',
     'SQLpass' => 'ATvtQEQN9Y4n'
