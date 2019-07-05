682c8500e45f6ca120842e89b0cf3ef811c2c418
Jamo <develop@nighttimedev.com>
2019-07-05 10:14:37 +0200

    working on monitor

--- monitor/monitor.php
+++ monitor/monitor.php
@@ -1,9 +1,4 @@
 <?php
-  function systemLoadInPercent($coreCount = 2,$interval = 1){
-      $rs = sys_getloadavg();
-      $interval = $interval >= 1 && 3 <= $interval ? $interval : 1;
-      $load  = $rs[$interval];
-      return round(($load * 100) / $coreCount,2);
-  }
-  echo systemLoadInPercent();
+  $load = sys_getloadavg();
+  echo $load[0];
 ?>
