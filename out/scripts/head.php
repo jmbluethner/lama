123b9c1e13f812238e5f68b7d5962b90882537bc
Jamo <develop@nighttimedev.com>
2019-07-24 13:19:23 +0200

    added first stages of /out/simple-manage

---
+++ out/scripts/head.php
@@ -0,0 +1,4 @@
+<link reL="stylesheet" href="../styles/out.css">
+<title>LAMA Server Manager | OUT</title>
+<meta charset="utf-8">
+<link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
--- out/simple-manage/index.php
+++ out/simple-manage/index.php
@@ -1,1 +1,31 @@
 <!-- Here you'll have to enter the PIN for the Server you want to control -->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include '../scripts/head.php' ?>
+    </head>
+    <body>
+        <div class="out_login_bg">
+            <div class="out_login_overlay">
+                <div class="out_login_toast">
+                    <h1>Want to manage A Server?</h1>
+                    <hr>
+                    <form action="check-pin.php" method="post">
+                        <input placeholder="Enter Server Pin" class="serverpin">
+                        <button type="submit" class="submit">Check my pin</button>
+                    </form>
+                    <?php
+                        if(isset($_GET['login_error'])) {
+                        ?>
+                        <div class="error_pin">
+                            <?php echo $_GET['login_error'] ?>
+                        </div>
+                        <?php
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
