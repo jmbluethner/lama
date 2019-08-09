28ff87ae93592ad29e2fbdf65e5b616bf36f78b5
Jamo <develop@nighttimedev.com>
2019-08-09 10:42:29 +0200

    worked on /install/setup

---
+++ install/setup/index.php
@@ -0,0 +1,52 @@
+<?php
+
+    /*
+     * Give the user the ability to install lama without touching the config.php
+     * Currently just a prototype / testing snippet
+     * 
+     */
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LAMA Server Manager - SETUP for Installation</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+        <meta name="author" content="JΔMØ,NTD">
+        <meta name="description" content="The advanced CS:GO Server Interface">
+        <meta property="og:image" content="../../favicon.ico">
+        <meta property="og:description" content="The advanced CS:GO Server Interface">
+        <meta property="og:title" content="LAN Server Manager">
+        <meta name="revisit-after" content="15 days">
+        <meta name="robots" content="index,follow">
+        <meta name="keywords" content="Lan, Intranet, DIE-LAN, Interface, Query, Source, CS:GO, Counter-Strike">
+        <meta itemprop="copyrightHolder" content="Jamo">
+        <meta itemprop="copyrightYear" content="2019">
+        <meta itemprop="isFamilyFriendly" content="True">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-title" content="LAN Server Manager">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="orange">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <link rel="shortcut icon" href="../../favicon.ico">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.css">
+        <link rel="stylesheet" href="./install.css" type="text/css">
+        <meta name="theme-color" content="#be0000">
+        <link rel="manifest" href="/manifest.json">
+        <!-- Fontawesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.1/css/all.css" type="text/css">
+    </head>
+    <body>
+        <div class="box">
+            <div class="sidebar">
+    test
+            </div>
+            <div id="sidebar_triangles"><span>&#9654;</span></div>
+            <div id="box_details">
+                test
+            </div>
+        </div>
+    </body>
+</html>
